0d2156012bd9388b2aa9dc8a45dd1cbb4e01865f
Lizards <amanda@smart.net>
2024-08-06 00:31:56 -0400

    git clone arch-system-config + attempt to check out branch name

--- chroot/dotfiles.sh
+++ chroot/dotfiles.sh
@@ -5,27 +5,27 @@ function main() {
     local HOSTNAME=${1}
     local USERNAME=${2}
     local VIRTUALBOX=${3}
+    if [ "${VIRTUALBOX}" == "1" ]; then
+        # Branches are named after machine's hostname; just do this to check out the virtualbox branches
+        HOSTNAME='virtualbox'
+    fi
     local USER_HOME
     USER_HOME="$(getent passwd "${USERNAME}" | cut -d: -f6)"
 
-    sudo -u "${USERNAME}" mkdir /tmp/arch-system-config
-    pushd /tmp/arch-system-config
-        sudo -u "${USERNAME}" curl -L https://github.com/Lizards/arch-system-config/tarball/master | tar -xvz --strip-component=1
-        sudo -u "${USERNAME}" aur build -d custom  -- --clean --syncdeps --noconfirm --needed
-        # Try to install system-specific config based on hostname, falling back to the shared base-config package only
-        pacman -Syu --noconfirm "${HOSTNAME}-config" || pacman -Syu --noconfirm base-config
-    popd
-
     pushd "${USER_HOME}"
+        sudo -u "${USERNAME}" git clone https://github.com/Lizards/arch-system-config
+        pushd arch-system-config
+            sudo -u "${USERNAME}" git checkout "${HOSTNAME}" || echo "No branch matching hostname ${HOSTNAME}; using master"
+            sudo -u "${USERNAME}" aur build -d custom  -- --clean --syncdeps --noconfirm --needed
+            # Try to install system-specific config based on hostname, falling back to the shared base-config package only
+            pacman -Syu --noconfirm --overwrite "*" "${HOSTNAME}-config" || pacman -Syu --noconfirm --overwrite "*" base-config
+        popd
+
         sudo -u "${USERNAME}" git clone --recurse-submodules -j8 https://github.com/Lizards/dotfiles
         pushd dotfiles
             echo
             echo "Installing dotfiles..."
-            if [ "${VIRTUALBOX}" == "1" ]; then
-                # Branches are named after machine's hostname; just do this to check out the virtualbox branch
-                HOSTNAME='virtualbox'
-            fi
-            sudo -u "${USERNAME}" git checkout "${HOSTNAME}" || echo "No branch matching hostname; using master"
+            sudo -u "${USERNAME}" git checkout "${HOSTNAME}" || echo "No branch matching hostname ${HOSTNAME}; using master"
             sudo -u "${USERNAME}" make
         popd
     popd
